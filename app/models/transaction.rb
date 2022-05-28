4fbc0c5eea2ce616c031d9d87fcd7c50fc72c0ea
James Okunlade <james.okunlade@gmail.com>
2022-05-28 12:41:08 +0100

    Validated transaction model params and added validation specs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,10 @@
 class Transaction < ApplicationRecord
+  # Validations
+  validates_presence_of :created_by_id, :input_amount_currency, :input_amount, :output_amount_currency,
+    :output_amount, :date_of_transaction
+
+  before_save do
+    self.input_amount_currency.downcase!
+    self.output_amount_currency.downcase!
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation tests
+  it { should validate_presence_of(:created_by_id) }
+  it { should validate_presence_of(:input_amount_currency) }
+  it { should validate_presence_of(:input_amount) }
+  it { should validate_presence_of(:output_amount_currency) }
+  it { should validate_presence_of(:output_amount) }
+  it { should validate_presence_of(:date_of_transaction) }
 end
